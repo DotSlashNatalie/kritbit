606279c2ade1b3a47032c587e943d5d310397cbf
Nathan Adams <adamsna@datanethost.net>
2015-11-14 16:19:37 -0600

    Adding config.dist.php

---
+++ web/application/config.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$config["ADMINS"] = array("adamsna@datanethost.net");
+
+$config["DATABASE_TYPE"] = "SQLITE";
+$config["DATABASE_FILE"] = "kritbot.sqlite3";
+
+$config["GOOGLE_OAUTH_ID"] = "";
+$config["GOOGLE_OAUTH_SECRET"] = "";
+
+return $config;
